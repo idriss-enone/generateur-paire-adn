03bcd3d26a8ba26340eab504de7d9a06cd9f9947
Idriss E <enonedjabe@gmail.com>
2026-01-14 17:54:29 +0100

    Ajout de tests simples avec console.log pour valider la fonction pairElement

--- script.js
+++ script.js
@@ -18,3 +18,10 @@ function pairElement(str){
   
   return result
 }
+
+console.log(pairElement("ATCGA"));
+// [["A","T"],["T","A"],["C","G"],["G","C"],["A","T"]]
+console.log(pairElement("TTGAG"));
+// [["T","A"],["T","A"],["G","C"],["A","T"],["G","C"]]
+console.log(pairElement("CTCTA"));
+// [["C","G"],["T","A"],["C","G"],["T","A"],["A","T"]]
